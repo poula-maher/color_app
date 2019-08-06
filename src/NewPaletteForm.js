0564efe8326311330636f7649d288ea6c95cc267
Poula Maher <poula_maher@outlook.com>
2019-08-07 00:15:17 +0200

    few changes

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -144,6 +144,7 @@ class NewPaletteForm extends Component {
             removeColor={this.removeColor}
             axis="xy"
             onSortEnd={this.onSortEnd}
+            distance={20}
           />
         </main>
       </div>
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,17 +1,31 @@
+import sizes from "./sizes";
+import bg from "./bg.svg";
 export default {
+  "@global": {
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 500ms ease-out"
+    }
+  },
   root: {
     backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#1805aa",
+    backgroundImage: `url(${bg})`
   },
   container: {
     width: "50%",
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("lg")]: {
+      width: "80%"
+    }
   },
   nav: {
     display: "flex",
@@ -28,6 +42,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "5%"
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2,50%)"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1,100%)",
+      gridGap: "1rem"
+    }
   }
 };
